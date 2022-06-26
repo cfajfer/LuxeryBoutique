c5dff5ea90f66eb5c269cd3ef766240753ba9f44
Corey Fajfer <corey@wealthcharts.com>
2022-06-26 08:59:46 -0400

    fix: remove backend from git repo

--- backend/src/api/designer/controllers/designer.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- *  designer controller
- */
-
-const { createCoreController } = require('@strapi/strapi').factories;
-
-module.exports = createCoreController('api::designer.designer');
