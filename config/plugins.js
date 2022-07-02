337dd8e5df252db35f2d3c244438535dd0dd747d
Corey Fajfer <corey@wealthcharts.com>
2022-07-02 11:45:38 -0400

    feature: cloudinary config

--- config/plugins.js
+++ config/plugins.js
@@ -1,4 +1,4 @@
-module.exports = () => ({
+module.exports = ({env}) => ({
   graphql: {
     enabled: true,
     config: {
@@ -10,5 +10,20 @@ module.exports = () => ({
         introspection: true,
       },
     }
-  }
+  },
+  upload: {
+    config: {
+      provider: 'cloudinary',
+      providerOptions: {
+        cloud_name: env('CLOUDINARY_NAME'),
+        api_key: env('CLOUDINARY_KEY'),
+        api_secret: env('CLOUDINARY_SECRET'),
+      },
+      actionOptions: {
+        upload: {},
+        uploadStream: {},
+        delete: {},
+      },
+    },
+  },
 })
