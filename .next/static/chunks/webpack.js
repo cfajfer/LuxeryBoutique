a3da13449f5d7ee65fb793d446371d09d94278aa
Corey Fajfer <corey@wealthcharts.com>
2022-07-18 18:06:19 -0400

    feature: new favicon and site title

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -161,7 +161,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "285cf74f06b81450"; }
+/******/ 		__webpack_require__.h = function() { return "e22032c238bd2fb5"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
